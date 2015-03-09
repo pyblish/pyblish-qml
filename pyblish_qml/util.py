c0740d31218c15424ce919a4b732bfb9c43ebb82
Marcus Ottosson <konstruktion@gmail.com>
2015-03-09 17:57:07 +0000

    Added where() for PATH traversal and discovery.

--- pyblish_qml/util.py
+++ pyblish_qml/util.py
@@ -1,3 +1,4 @@
+import os
 import time
 import logging
 
@@ -53,3 +54,35 @@ class Log(QtCore.QObject):
     @QtCore.pyqtSlot(str)
     def error(self, msg):
         self.log.error(msg)
+
+
+def where(program):
+    """Parse PATH for executables
+
+    Windows note:
+        PATHEXT yields possible suffixes, such as .exe, .bat and .cmd
+
+    Usage:
+        >>> where("python")
+        c:\python27\python.exe
+
+    """
+
+    suffixes = [""]
+
+    try:
+        # Append Windows suffixes, such as .exe, .bat and .cmd
+        suffixes.extend(os.environ.get("PATHEXT").split(os.pathsep))
+    except:
+        pass
+
+    for path in os.environ["PATH"].split(os.pathsep):
+
+        # A path may be empty.
+        if not path:
+            continue
+
+        for suffix in suffixes:
+            full_path = os.path.join(path, program + suffix)
+            if os.path.isfile(full_path):
+                return full_path
