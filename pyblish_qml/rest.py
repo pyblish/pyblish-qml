3738876a57798e17eda8a252382ebd7ab568d2a1
Marcus Ottosson <konstruktion@gmail.com>
2015-03-09 17:57:28 +0000

    Deferring heavy import of requests library.

--- pyblish_qml/rest.py
+++ pyblish_qml/rest.py
@@ -1,5 +1,4 @@
 import json
-from vendor import requests
 
 ADDRESS = "http://127.0.0.1:{port}/pyblish/v1{endpoint}"
 PORT = 6000
@@ -7,6 +6,7 @@ MOCK = None
 
 
 def request(verb, endpoint, data=None, **kwargs):
+    from vendor import requests
     endpoint = ADDRESS.format(port=PORT, endpoint=endpoint)
 
     request = getattr(MOCK or requests, verb.lower())
