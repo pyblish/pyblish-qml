ab91875721f4a4a4435ea00f7088740632829072
Marcus Ottosson <konstruktion@gmail.com>
2015-03-27 11:18:29 +0000

    Updated dependency requirements

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
         "pyblish_qml": qml_package_data
     },
     entry_points={},
-    install_requires=["pyblish-endpoint>=1.1.6",
-                      "pyblish>=1.0.13"]
+    install_requires=["pyblish-endpoint>=1.1.7",
+                      "pyblish>=1.0.14"]
 )
