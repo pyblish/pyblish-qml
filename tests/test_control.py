0fa8c628aacd7da5319b6d88771c91b78ef07250
Marcus Ottosson <konstruktion@gmail.com>
2016-02-17 21:09:56 +0000

    Add test for fix for #157

--- tests/test_control.py
+++ tests/test_control.py
@@ -243,6 +243,41 @@ def test_cooperative_collection():
 
 
 @with_setup(lib.clean)
+def test_cooperative_collection2():
+    """Cooperative collection works with InstancePlugin"""
+
+    count = {"#": 0}
+    history = []
+
+    class CollectorA(pyblish.api.ContextPlugin):
+        order = pyblish.api.CollectorOrder
+
+        def process(self, context):
+            history.append(type(self).__name__)
+            context.create_instance("MyInstance")
+            count["#"] += 1
+
+    class CollectorB(pyblish.api.InstancePlugin):
+        order = pyblish.api.CollectorOrder + 0.1
+
+        def process(self, instance):
+            history.append(type(self).__name__)
+            count["#"] += 10
+
+    pyblish.api.register_plugin(CollectorA)
+    pyblish.api.register_plugin(CollectorB)
+
+    c = reset()
+
+    check_present("CollectorA", c.item_model)
+    check_present("CollectorB", c.item_model)
+    check_present("MyInstance", c.item_model)
+
+    assert count["#"] == 11, count
+    assert history == ["CollectorA", "CollectorB"]
+
+
+@with_setup(lib.clean)
 def test_published_event():
     """published is emitted upon finished publish"""
 
