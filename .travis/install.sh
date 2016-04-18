b891513fb6597179c5cb572daa4df627d03db412
Marcus Ottosson <konstruktion@gmail.com>
2016-04-18 11:27:55 +0100

    Removing fuzz, fixing sip

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,7 @@ wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.
 wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
 
 echo "Building sip.."
-tar xvzf sip-4.17.tar.gz
+tar xzf sip-4.17.tar.gz
 cd sip-4.17
 python configure.py
 make
@@ -15,9 +15,9 @@ sudo make install
 cd ..
 
 echo "Building PyQt5.."
-tar xvzf PyQt-gpl-5.5.1.tar.gz
+tar xzf PyQt-gpl-5.5.1.tar.gz
 cd PyQt-gpl-5.5.1
-python configure.py --sip=$SIP --confirm-license
+python configure.py --confirm-license
 make
 sudo make install
 cd ..
