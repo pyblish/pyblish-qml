63d8101e37aa43c3f62ca9b592d03f6ea1bf9ce6
Marcus Ottosson <konstruktion@gmail.com>
2016-06-19 15:13:27 +0100

    Add --demo option to CLI

--- pyblish_qml/__main__.py
+++ pyblish_qml/__main__.py
@@ -8,12 +8,16 @@ from . import app
 
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--validate", action="store_true")
+    parser.add_argument("--demo", action="store_true",
+                        help="Illustrate features of the GUI")
+    parser.add_argument("--debug", action="store_true",
+                        help="Development mode")
+    parser.add_argument("--validate", action="store_true",
+                        help="Check system prior to launching")
 
     kwargs = parser.parse_args()
 
-    return app.main(debug=kwargs.debug,
+    return app.main(debug=kwargs.debug or kwargs.demo,
                     validate=kwargs.validate)
 
 
