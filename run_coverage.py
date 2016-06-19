a540e08b1299b0c78c95288a91a8b4c61b62cbd4
Marcus Ottosson <konstruktion@gmail.com>
2016-06-19 18:30:25 +0100

    Reduce cruft

--- run_coverage.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    import nose
-    argv = sys.argv[:]
-    argv.extend(["-c", ".noserc"])
-    nose.main(argv=argv)
--- run_testsuite.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-if __name__ == "__main__":
-    import nose
-    argv = sys.argv[:]
-    argv.extend(["--exclude=vendor", "--with-doctest", "--verbose"])
-    nose.main(argv=argv)
