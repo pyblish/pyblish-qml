c398c966d45ed80edd2c5e409f3c82d5b0db8d59
Marcus Ottosson <konstruktion@gmail.com>
2017-09-10 08:04:26 +0100

    Fix #261

--- pyblish_qml/ipc/formatting.py
+++ pyblish_qml/ipc/formatting.py
@@ -267,7 +267,10 @@ def format_plugin(plugin):
         "optional": plugin.optional,
         "hosts": plugin.hosts,
         "families": plugin.families,
-        "targets": plugin.targets,
+
+        # New in pyblish-base 1.5.2
+        "targets": getattr(plugin, "targets", list()),
+
         "doc": inspect.getdoc(plugin),
         "active": plugin.active,
         "match": plugin.match,
