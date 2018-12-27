00bd1e47a4b4e499822c9d94cee51df3e1f6f907
David Lai <david962041@gmail.com>
2018-12-27 21:51:24 +0800

    Print deprecation warning message

--- pyblish_qml/__init__.py
+++ pyblish_qml/__init__.py
@@ -5,8 +5,11 @@ from .version import (
 )
 
 
-def show(parent=None, targets=None, modal=None):
+def show(parent=None, targets=None, modal=None, foster=None):
     from . import host
+
+    if foster is not None:
+        print("Foster Mode has been deprecated.")
 
     if targets is None:
         targets = []
