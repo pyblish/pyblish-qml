31870fbd920927562d7459fe9f69f1cfe0b45bf9
Marcus Ottosson <konstruktion@gmail.com>
2019-02-14 11:50:07 +0000

    Improve path handling for PyQt5

--- pyblish_qml/ipc/server.py
+++ pyblish_qml/ipc/server.py
@@ -343,6 +343,23 @@ def find_pyqt5(python):
         os.getenv("PYBLISH_QML_PYQT5")
     )
 
+    # If not registered, ask Python for it explicitly
+    # This avoids having to expose PyQt5 on PYTHONPATH
+    # where it may otherwise get picked up by bystanders
+    # such as Python 2.
+    if not pyqt5:
+        try:
+            path = subprocess.check_output([
+                python, "-c",
+                "import PyQt5, sys;"
+                "sys.stdout.write(PyQt5.__file__)"
+            ])
+
+            pyqt5 = os.path.dirname(path)
+
+        except subprocess.CalledProcessError:
+            pass
+
     return pyqt5
 
 
